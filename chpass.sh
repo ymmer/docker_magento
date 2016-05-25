33561406adc6f9cbb18eeba764979e5e527615b7
ymmer <marcel.remmy@alumni.fh-aachen.de>
2016-05-25 14:59:49 +0200

    chpasswd for magento sftp

---
+++ chpass.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function randomString {
+        # if a param was passed, it's the length of the string we want
+        if [[ -n $1 ]] && [[ "$1" -lt 20 ]]; then
+                local myStrLength=$1;
+        else
+                # otherwise set to default
+                local myStrLength=8;
+        fi
+
+        local mySeedNumber=$$`date +%N`; # seed will be the pid + nanoseconds
+        local myRandomString=$( echo $mySeedNumber | md5sum | md5sum );
+        # create our actual random string
+        myRandomResult="${myRandomString:2:myStrLength}"
+}
+
+randomString 11;
+
+echo "###################";
+echo "# SFTP    :808<id>";
+echo "# user	magento";
+echo "# pass	$myRandomResult";
+echo "###################";
+
+echo "magento:$myRandomResult" | chpasswd
